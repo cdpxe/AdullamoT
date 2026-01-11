8c1a5e423a81737a67120939bc391274572272e3
Steffen Wendzel <wendzel@hs-worms.de>
2026-01-11 16:18:36 +0100

    simplifications

--- block_recv.sh
+++ block_recv.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-#BLOCK SB 100
+# BLOCK SB 100
+
 PRINTER_IP="192.168.2.220"
-RECV_FILE=block.rcv
 
-source block_shared.sh
-
-if [ ! -e $RECV_FILE ]; then
-	touch $RECV_FILE
-fi
-
-nc $PRINTER_IP 514 2>/dev/null | grep "fsfsFlashFileHandleOpen: File" | sed 's/.*flash:\/\///g' 
-
-#	if [ "$RECV_MSG" == "EOF" ]; then
-#		echo "Recv'd EOF. Existing"
-#	fi
-#	echo " MSG=$RECV_MSG"
+nc $PRINTER_IP 514 2>/dev/null | grep "fsfsFlashFileHandleOpen: File" #| sed 's/.*flash:\/\///g' 
 
--- block_send.sh
+++ block_send.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#BLOCK SB 100
+# BLOCK SB 100
+
 PRINTER_IP="192.168.2.220"
 
 if [ "$1" != "" ]; then
@@ -8,19 +9,13 @@ else
 	INPUT_SECRET_MSG="KATZE"
 fi
 
-source block_shared.sh
-
 # 1. transfer string into url format
 INPUT_SECRET_MSG=`urlencode $INPUT_SECRET_MSG`
-#echo "INPUT_SECRET_MSG=$INPUT_SECRET_MSG"
-#read go
+
 # 2. split into chunks of the max. possible string length (59 characters)
 echo "Sending chunks of the secret msg ..."
 for SECRET_MSG in `echo $INPUT_SECRET_MSG | fold -w59`; do
-	#echo "Next chunk='"$SECRET_MSG"'."
-	RECV_MSG="notset"
-	# parameters: printer-ip  csrf  secret_msg
-	post_message $PRINTER_IP "unused" $SECRET_MSG
+	GET "http://"$PRINTER_IP"/"$SECRET_MSG >/dev/null
 	echo -n "#"
 done
 echo "done."
